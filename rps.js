d6ef28a784c41999fa137fb11f65161df1e227e5
adi <xdragon.0601@outlook.com>
2022-05-30 15:20:16 +0530

    Add display methods for score and current hand

--- rps.js
+++ rps.js
@@ -27,20 +27,58 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+const pHand = document.querySelector('.player-hand');
+const cHand = document.querySelector('.computer-hand')
+
+function displayHands(pChoice, cChoice) {
+    pHand.textContent = `You played ${pChoice}`
+    cHand.textContent = `Computer played ${cChoice}`
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+function updateScore(winner) {
+
+    winner = winner.toLowerCase();
+
+    if (winner == 'player') {
+        playerScore += 1;
+        return 'You Won this round!';
+    } else if (winner == 'computer'); {
+        computerScore += 1;;
+        return 'Computer won this round!';
+    } 
+}
+
+const roundResult = document.querySelector('.round-result');
+const liveScore = document.querySelector('.live-score')
+
+function displayScore(msg) {
+    roundResult.textContent = msg
+    liveScore.textContent = `${playerScore} : ${computerScore}`
+}
 
 function playGame(e) {
     let playerChoice = e.target.getAttribute('data-value');
     let computerChoice = computerPlay();
 
     let winner = playRound(playerChoice, computerChoice);
+
+    displayHands(playerChoice, computerChoice);
+    let roundMsg = updateScore(winner);
+    displayScore(roundMsg);
     console.log(winner)
 
     // TODO:
-    // -updateDisplay() for updating score
+    // -updateDisplay() for updating score and updateHand() as well maybe
     // -win logic for n rounds
     // -reset button to start a fresh game
+    // work on logic/dynamic stuff first before styling/css or aligning stuff
 
 }
+
 
 const rpsButtons = document.querySelectorAll('.rps');
 
